46fb900a3f9a66509074907d8ece035272c38baa
Rewa Patil <>
2021-05-04 23:49:31 +0000

    changed quote

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 document.getElementById('calculate').addEventListener('click', input);
 
 function input() {
-  let const = parseInt(document.getElementById('input').value;)
+  let const = parseInt(document.getElementById('input').value);
   let const = number * number;
   document.querySelector ('.answer').innerText = 'The Answer = ' + ans;
 }
