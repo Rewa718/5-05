1085535889e84472e6c6b58053870badcd843562
Rewa Patil <>
2021-05-05 16:54:00 +0000

    change variable

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('calculate').addEventListener('click', square)
 
 function square () {
-  const = parseInt(document.getElementById('input').value)
+  let input = parseInt(document.getElementById('input').value)
   let number = num * num
   document.querySelector('.answer').innerText = 'The Answer = ' + answer
 }
