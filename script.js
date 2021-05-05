f80f2f88c10930eff9f7c399b5f6824b6354dab3
Rewa Patil <>
2021-05-05 16:09:45 +0000

    js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('calculate').addEventListener('click', square)
 
 function square () {
-  let num = parseInt(document.getElementById('input').value);
-  var area = num * num
-  document.querySelector('.answer').innerText = 'The Answer = ' + ans
+  const = parseInt(document.getElementById('input').value)
+  let number = num * num
+  document.querySelector('.answer').innerText = 'The Answer = ' + answer
 }
