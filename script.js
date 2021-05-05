93fc4c77c76c6fa42176c7386bea5031dfc70946
Rewa Patil <>
2021-05-05 18:23:57 +0000

    remove double quote

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
-document.getElementById('calculate').addEventListener('click', myFunction);
+document.getElementById('calculate').addEventListener('click', myFunction)
 
-function myFunction() {
- let num = parseInt(document.getElementById('userinput').value);
- let ans = num * num; 
- document.getElementById('displayans').innerText = "Answer =" + ans;
+function myFunction () {
+  let num = parseInt(document.getElementById('userinput').value)
+  let ans = num * num; 
+  document.getElementById('displayans').innerText = 'Answer =' + ans
 }
