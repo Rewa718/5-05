79059903c4434a801bdb4a1714b2be7714826712
Rewa Patil <>
2021-05-05 00:13:49 +0000

    correct js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 
 
-document.getElementById('calculate').addEventListener('click', square);
+document.getElementById('calculate').addEventListener('click', square)
 
-function square() {
-  let num = parseInt(document.getElementById('input').value);
-  let ans = num * num;
-  document.querySelector ('.answer').innerText = 'The Answer = ' + ans;
+function square () {
+  const = parseInt(document.getElementById('input').value);
+  const = num * num
+  document.querySelector('.answer').innerText = 'The Answer = ' + ans
 }
