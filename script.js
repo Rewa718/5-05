cfec7a534569221a335cc588f74512a5960ef016
Rewa Patil <>
2021-05-05 14:32:45 +0000

    Add variable

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ document.getElementById('calculate').addEventListener('click', square)
 
 function square () {
   let num = parseInt(document.getElementById('input').value);
-  const = num * num
+  var area = num * num
   document.querySelector('.answer').innerText = 'The Answer = ' + ans
 }
